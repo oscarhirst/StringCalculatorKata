8f0089ec2bfbdbc165713991e5d480d2ad91e89e
oscarhirst <10322447+oscarhirst@users.noreply.github.com>
2020-02-12 10:34:27 +0000

    set up empty project

---
+++ BowlingTests/BowlingTests.cs
@@ -0,0 +1,23 @@
+// <copyright file="BowlingTests.cs" company="Corsham Science">
+// Copyright (c) Corsham Science. All rights reserved.
+// </copyright>
+
+namespace Bowling.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class BowlingTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
