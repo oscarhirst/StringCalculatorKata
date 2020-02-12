26f0f48a851e3834802bfc1ae88238db72fa6305
oscarhirst <10322447+oscarhirst@users.noreply.github.com>
2020-02-12 16:59:17 +0000

    returns single digits or product of inputs

--- StringCalculator/StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator/StringCalculator.cs
@@ -1,11 +1,46 @@
 ﻿// <copyright file="StringCalculator.cs" company="Corsham Science">
 // Copyright (c) Corsham Science. All rights reserved.
 // </copyright>
+// based on exercise http://www.peterprovost.org/blog/2012/05/02/kata-the-only-way-to-learn-tdd/
 
 namespace StringCalculator
 {
+    using System;
+    using System.Linq;
+
     public class StringCalculator
     {
-        public int Calculate(string input) => 0;
+        public int Calculate(string input)
+        {
+            // req 1
+            if (input == string.Empty)
+            {
+                return 0;
+            }
+
+            // req2
+            if (int.TryParse(input, out var number))
+            {
+                return number;
+            }
+
+            // req 3
+            var inputs = input.Split(',');
+
+            // var numbers = input.Split(',').Select(n =>
+            // {
+            //    if (!int.TryParse(n, out var number))
+            //    {
+            //        throw new InvalidOperationException();
+            //    }
+            //    return number;
+            // });
+            if (!int.TryParse(inputs[0], out var number1) || !int.TryParse(inputs[1], out var number2))
+            {
+                throw new InvalidOperationException();
+            }
+
+            return number1 * number2;
+        }
     }
 }
--- StringCalculator/StringCalculatorTests/StringCalculatorTests.cs
+++ StringCalculator/StringCalculatorTests/StringCalculatorTests.cs
@@ -4,6 +4,7 @@
 
 namespace StringCalculatorTests
 {
+    using System;
     using NUnit.Framework;
     using StringCalculator;
 
@@ -17,10 +18,34 @@ namespace StringCalculatorTests
             _calculator = new StringCalculator();
         }
 
+        [TestCase("    ")]
+        [TestCase("blablabla")]
+        [TestCase("34343434abcdefgh999")]
+        public void Calculate_GivenNonNumericString_ThrowsInvalidOperationException(string input)
+        {
+            Assert.Throws(typeof(InvalidOperationException), () => { _calculator.Calculate(input); });
+        }
+
         [Test]
         public void Calculate_GivenEmptyString_Returns0()
         {
             Assert.AreEqual(0, _calculator.Calculate(string.Empty));
+        }
+
+        [TestCase(1)]
+        [TestCase(6)]
+        [TestCase(9)]
+        public void Calculate_GivenSingleNumber_ReturnsInput(int input)
+        {
+            Assert.AreEqual(input, _calculator.Calculate(input.ToString()));
+        }
+
+        [TestCase(1, 5)]
+        [TestCase(6, 32)]
+        [TestCase(9020, 223)]
+        public void Calculate_GivenSingleNumber_ReturnsInput(int input1, int input2)
+        {
+            Assert.AreEqual(input1 * input2, _calculator.Calculate($"{input1},{input2}"));
         }
     }
 }
